a916907b53ba08477fc064f2300df2bb581436e2
julesgaudet <julesgaudet@hotmail.com>
2023-11-30 13:45:22 -0500

    jai fini la premiere fonction dans circuit_IO

--- circuit_IO.c
+++ circuit_IO.c
@@ -17,47 +17,75 @@ Derni�re modification  :
 void circuit_IO_sauvegarder(const char *nom_fichier, const t_circuit *circuit) {
     FILE *fichier = fopen(nom_fichier, "w");
     int i;
-    char resultat[100];
+    char resultat[1000];
 
     if (fichier == NULL) {
         printf("Erreur, le fichier est NULL\n");
         return;
     }
 
-    // Écriture du nombres d'entrées, de sorties et de portes
+    //Écriture du nombre d'entrées, de sorties et de portes
     fprintf(fichier, "%d %d %d\n", t_circuit_get_nb_entrees(circuit),
                                    t_circuit_get_nb_sorties(circuit), 
                                    t_circuit_get_nb_portes(circuit));
 
-    // Écriture des entrées
+    //Écriture des entrées
     for (i = 0; i < t_circuit_get_nb_entrees(circuit); i++) {
         t_entree_serialiser(circuit->entrees[i], resultat);
         fprintf(fichier, "%s\n", resultat);
     }
 
-    // Écriture des sorties
+    //Écriture des sorties
     for (i = 0; i < t_circuit_get_nb_sorties(circuit); i++) {
         t_sortie_serialiser(circuit->sorties[i], resultat);
         fprintf(fichier, "%s\n", resultat);
     }
 
-    // Écriture des portes
+    //Écriture des portes
     for (i = 0; i < t_circuit_get_nb_portes(circuit); i++) {
         t_porte_serialiser(circuit->portes[i], resultat);
         fprintf(fichier, "%s\n", resultat);
     }
 
-    //Il manque le reste 
-    //Est-ce qu'il faut utiliser t_pin_entree_get_lien?
+    //Écriture des liens
+    for (i = 0; i < t_circuit_get_nb_portes(circuit); i++) {
+        fprintf(fichier, "%s ", t_porte_get_nom(circuit->portes[i]));
+
+        //Imprimer l'entrée de la porte
+        for (int j = 0; j < t_porte_get_nb_entrees(circuit->portes[i]); j++) {
+            
+            //S'il n'y a pas d'entrées, ajouter XX
+            if (t_porte_get_pin_entree(circuit->portes[i], j) == NULL) {
+                fprintf(fichier, "XX ");
+            }
+
+            //S'il y a une entrée pour la porte
+            else {
+                fprintf(fichier, "%s ", t_porte_get_pin_entree(circuit->portes[i], j));
+            }
+        }
+
+        //Ajouter un saut de ligne pour la prochaine porte
+        fprintf(fichier, "\n");
+    }
+
+    //Ajouter les dernières pins de sorties avec les pins d'entrées
+    for (i = 0; i < t_circuit_get_nb_sorties(circuit); i++) {
+        fprintf(fichier, "%s ", t_sortie_get_nom(circuit->sorties[i]));
+
+        //Ajouter la porte associée à la sortie
+        fprintf(fichier, "%s\n", t_sortie_get_porte_nom(circuit->sorties[i]));
+    }
 
     fclose(fichier);
 }
+
 
 /*****************************************************************************/
 
 void circuit_IO_charger(const char *chemin_acces, t_circuit *circuit)
 {
-
+    
 }
 
 /*****************************************************************************/
--- circuit_IO.h
+++ circuit_IO.h
@@ -15,13 +15,16 @@ Derni�re modification:
 #include "t_circuit.h"
 #include "t_entree.h"
 #include "t_sortie.h"
+#include "t_porte.h"
 
 //**************************************************************************************//
 
 /*
 Fonction: circuit_IO_sauvegarder
 
-Description: Sauvegarde les composantes d'un circuit en format textuel. Les entrées, sorties, portes et liaisons sont sérialisées et écrites dans un fichier texte. Pendant l'écriture, des échos sont affichés dans la console pour valider l'état du circuit.
+Description: Sauvegarde les composantes d'un circuit en format textuel. 
+Les entrées, sorties, portes et liaisons sont sérialisées et écrites dans 
+un fichier texte.
 
 Paramètres:
 - nom_fichier: Nom du fichier dans lequel sauvegarder le circuit.
@@ -34,7 +37,9 @@ void circuit_IO_sauvegarder(const char * nom_fichier, const  t_circuit * circuit
 /*
 Fonction: circuit_IO_charger
 
-Description: Charge un circuit à partir d'un fichier texte contenant la description textuelle du circuit. Les éléments du circuit (entrées, sorties, portes et liaisons) sont créés en utilisant les informations du fichier.
+Description: Charge un circuit à partir d'un fichier texte contenant 
+la description textuelle du circuit. Les éléments du circuit (entrées, 
+sorties, portes et liaisons) sont créés en utilisant les informations du fichier.
 
 Paramètres:
 - chemin_acces: Chemin d'accès du fichier à charger.
@@ -47,7 +52,9 @@ void circuit_IO_charger(const char *chemin_acces, t_circuit *circuit);
 /*
 Fonction: t_circuit_tdv
 
-Description: Calcule la table de vérité du circuit. La table de vérité est un tableau à deux dimensions où les colonnes représentent les entrées et les sorties, et les lignes représentent toutes les combinaisons de bits en entrée.
+Description: Calcule la table de vérité du circuit. La table de vérité 
+est un tableau à deux dimensions où les colonnes représentent les entrées 
+et les sorties, et les lignes représentent toutes les combinaisons de bits en entrée.
 
 Paramètres:
 - le_circuit: Pointeur vers la structure de circuit.
